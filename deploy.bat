f65ad868ab102cad22593cf5fb099f0d624e27e2
GoatXYX <cdcyuma@gmail.com>
2026-02-13 14:13:49 -0800

    Remove deploy script

--- deploy.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Deploying to GitHub Pages...
-git add .
-set /p commit_msg="Enter commit message (default: Update content): "
-if "%commit_msg%"=="" set commit_msg=Update content
-git commit -m "%commit_msg%"
-git push origin master
-echo Deployment complete!
-pause
